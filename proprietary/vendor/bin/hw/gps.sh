28834117ca5a798b5d9c2aa302cfcb5120a88dca
TheN001 <djole2k6@gmail.com>
2023-02-15 02:04:35 +0100

    test

--- proprietary/vendor/bin/hw/gps.sh
+++ proprietary/vendor/bin/hw/gps.sh
@@ -3,15 +3,15 @@
 SILENT_LOGGING_9900=/data/vendor/gps/silentGnssLogging
 SILENT_LOGGING_ISSUETRACKER=/data/vendor/gps/silentGnssLoggingIssueTracker
 
-CONFIGFILE=/vendor/etc/gnss/gps.cfg
+CONFIGFILE=/vendor/etc/gnss/gps.xml
 DAEMONFILE=/vendor/bin/hw/gpsd
 
 if [ -d "$SILENT_LOGGING_ISSUETRACKER" ] ; then 
-	CONFIGFILE=/vendor/etc/gnss/gps.debug.cfg
+	CONFIGFILE=/vendor/etc/gnss/gps.issuetracker.xml
 fi
 
 if [ -d "$SILENT_LOGGING_9900" ] ; then 
-	CONFIGFILE=/vendor/etc/gnss/gps.debug.cfg
+	CONFIGFILE=/vendor/etc/gnss/gps.debug.xml
 fi
 
 exec $DAEMONFILE -c $CONFIGFILE
